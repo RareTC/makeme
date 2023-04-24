9cd51982e629a4d2b1b89187587506bcb3dee6f9
RareTC <trevorjcampbell@aol.com>
2023-04-23 21:43:36 -0700

    Background adjusted for non log in page.

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -6,7 +6,7 @@ import LoginForm from '../../components/LoginForm/LoginForm';
 export default function AuthPage({ setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
   return (
-    <main>
+    <main className='auth-main'>
       <div className='auth-container'>
         <h1 className='auth'>Authorization Page</h1>
         <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
