6cd29534f3815aee873db7fc586bf80c0e10f970
RareTC <trevorjcampbell@aol.com>
2023-04-28 11:52:58 -0700

    Going to use buttons instead of prefilled text

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,7 +4,6 @@ import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
 import NavBar from '../../components/NavBar/NavBar';
-import MarkdownMaker from '../MarkdownMaker/MarkdownMaker';
 import Markdown from '../Markdown/Markdown';
 
 export default function App() {
@@ -15,9 +14,8 @@ export default function App() {
       { user ?
           <>
           <NavBar user={user} setUser={setUser} />
+          <Markdown />
             <Routes>
-              <Route path="/templates" element={<MarkdownMaker />} />
-              <Route path="/new" element={<Markdown />} />
             </Routes>
           </>
           :
