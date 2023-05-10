ba9a8fca389a7d9dc296ef996b9ae9f49956a4dc
RareTC <trevorjcampbell@aol.com>
2023-05-10 09:49:01 -0700

    Fixed Link in footer, changed CSS for body, and scroll bar visibility

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import './Footer.css';
@@ -7,17 +6,18 @@ import './Footer.css';
 export default function Footer() {
   return (
     <>
-    <div className='footer'>
+      <div className='footer'>
         <div className='footerlink'>
-            <Link to="https://www.linkedin.com/in/-trevorcampbell" target="_blank" rel="noopener noreferrer">
-                <LinkedInIcon className='footerlink'/>
-            </Link>
-            <Link to="https://github.com/RareTC" target='_blank' rel='noopener noreferrer'>
-                <GitHubIcon className='footerlink'/>
-            </Link>
+          <a href="https://www.linkedin.com/in/-trevorcampbell" target="_blank" rel="noopener noreferrer">
+            <LinkedInIcon className='footerlink' />
+          </a>
+          <a href="https://github.com/RareTC" target='_blank' rel='noopener noreferrer'>
+            <GitHubIcon className='footerlink' />
+          </a>
+
         </div>
         <p className='cc'> &copy; 2023 Trevor's Project</p>
-    </div>
+      </div>
     </>
   )
 }
